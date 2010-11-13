10e18f9bf5e05d464879f6e5024402d4c19265fa
ehouseoffers <ehouseoffers@28dev.com>
2010-11-13 11:49:23 -0800

    Dumping some cap variables into our logs (for testing deployments during development)

--- before_migrate.rb
+++ before_migrate.rb
@@ -1,4 +1,8 @@
 run "echo 'before_migrate.rb is running...'"
+run "echo 'node inspected: #{node.inspect}' >> #{shared_path}/logs.log"
 run "echo 'release_path: #{release_path}' >> #{shared_path}/logs.log"
-run "ln -nfs #{shared_path}/config/foo.yml #{release_path}/config/foo.yml"
-sudo "echo 'sudo works' >> /root/sudo.log"
+run "echo 'shared_path: #{shared_path}' >> #{shared_path}/logs.log"
+run "echo 'current_path: #{current_path}' >> #{shared_path}/logs.log"
+
+# run "ln -nfs #{shared_path}/config/foo.yml #{release_path}/config/foo.yml"
+# sudo "echo 'sudo works' >> /root/sudo.log"
