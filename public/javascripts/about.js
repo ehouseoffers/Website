082069f944e7186a5a44a78475d51a214c5746a4
ehouseoffers <ehouseoffers@28dev.com>
2010-11-19 14:13:00 -0800

    Final touches for tos on about page

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
     var trigger = $('a#tos_view_trigger');
-    console.log(trigger);
     var list = $('div#tos_veil');
-    console.log(list);
 
-    trigger.click(function(){
+    // Show tos onload?
+    if ( window.location.hash == '#tos' ) {
+        trigger.html('Hide Terms of Service');
+        list.removeClass('hidden');
+    }
+
+    // Setup tos show/hide toggle for both the trigger in the page as well as the footer tos link
+    $('a#footer_tos_link, a#tos_view_trigger').click(function(){
         if ( list.hasClass('hidden') ) {
             trigger.html('Hide Terms of Service');
             list.removeClass('hidden');
