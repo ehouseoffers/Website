4a964caaaa8cacdfe5ce990ee6e87c2d20f18ee7
ehouseoffers <ehouseoffers@28dev.com>
2010-12-02 23:45:58 -0800

    removing some old files

--- app/controllers/spotlight_controller.rb
+++
@@ -1,6 +0,0 @@
-class SpotlightController < ApplicationController
-  before_filter { |app_cont| app_cont.active_section(:spotlighters) }
-
-  before_filter :set_seller_listing,  :only => [:index]
-
-end
