cfdbe839335c5c69f4660cb96894a4031e00ac68
ehouseoffers <ehouseoffers@28dev.com>
2010-12-24 11:52:09 -0800

    Adding canonical_path and noydir,noodp meta data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,6 +62,25 @@ class ApplicationController < ActionController::Base
     @noindex = params[:page].to_i>1
   end
 
+  # Rules
+  # 1. the 'path' parameter is the ONLY parameter we accept. The main function of this is to weed out tracking
+  #    parameters. This means we construct canonical paths using a white list (things we accept, i.e. page) and not a
+  #    black list (things we do not accept, i.e. YSMKEY). This may introduce a maintenance nightmare going forward as
+  #    any new accepted parameter will have to be added to this. However, for the foreseeable future, pagination is
+  #    the only instance where params should be passed. In all other cases, params should be passed in the path itself
+  # 2. Page 1 of any paginated page is actually the page itself, so no page param is needed
+  def canonical_path
+    # root_url ends in a slash, request.path starts with one, we need to strip one of them out!
+    url = "#{root_url.gsub(/\/$/,'')}#{request.path}"
+
+    # rule #1 && rule #2
+    if params['page'] && params['page'].to_i != 1
+      url = url + "?page=#{params[:page]}"
+    end
+
+    url
+  end
+
   def encrypt(*args)
     YAML::dump(args).encrypt(:symmetric, :password => KEYS['encryption']['password'])
   end
