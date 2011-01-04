3fb1610c01c9fd3e43a6fa04d8a600baad3097f6
ehouseoffers <ehouseoffers@28dev.com>
2011-01-04 14:44:29 -0800

    Adding ckeditor (which replaced tiny_mce due to tiny's inability to upload images in the wysiwyg editor)

---
+++ public/ckeditor/config.js
@@ -0,0 +1,22 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+    config.toolbar_simple =
+    [
+        ['Templates','Table','-',
+         'Styles','Format','Font','FontSize','TextColor','-',
+         'Link','Unlink','Bold', 'Italic','Underline','Strike','-',
+         'Indent','Outdent','-',
+         'NumberedList','BulletedList','-',
+         'Undo','Redo', 'Link','Image','Source']
+    ];
+
+};
