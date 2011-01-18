e72a148b0f428b47bcc8e06a641a1aa211806605
ehouseoffers <ehouseoffers@28dev.com>
2011-01-18 14:01:16 -0800

    replaced ampersand entities with just an & (since rails automatically html encodes)

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,9 +2,9 @@ module BlogsHelper
   # Meta Title
   def title_for_context(pluralize=false)
     case @context
-    when "trends"  then "Real Estate, Short Sale &amp; Foreclosure Trends"
+    when "trends"  then "Real Estate, Short Sale & Foreclosure Trends"
     when "reasons" then "Sell My House or Property Fast"
-    when "guides"  then "How to Sell your House &amp; Avoid Foreclosure"
+    when "guides"  then "How to Sell your House & Avoid Foreclosure"
     end
   end
 
